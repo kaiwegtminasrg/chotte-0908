f9ccd5d3b59d2b3a0082ed333503088a7aa11c21
kaiwegtminasrg <127659583+kaiwegtminasrg@users.noreply.github.com>
2023-09-06 00:55:54 -0400

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'595d4fd7-06d2-4b2a-a3f6-935c7989d024'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
